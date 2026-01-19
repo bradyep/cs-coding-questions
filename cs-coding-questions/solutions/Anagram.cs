9dd4d6a69daf4ff4781f7bdff4af930de4e20039
Brady Prigge <bradyep@gmail.com>
2026-01-19 17:57:08 -0600

    feat: anagram class

---
+++ cs-coding-questions/solutions/Anagram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cs_coding_questions.solutions
+{
+  internal record PossibleAnagram(string firstWord, string secondWord);
+  internal class Anagram : Solution
+  {
+    const char WORD_PAIR_SEPARATOR = '-';
+    const char ANAGRAM_SET_SEPARATOR = '_';
+
+    public Anagram(Dictionary<string, string> solutionParams, bool? debug = false) : base(solutionParams, debug)
+    {
+      this.paramsAreValid = this.verifyParams();
+    }
+
+    public override bool verifyParams()
+    {
+      return true;
+    }
+
+    public override string[] solve(SolutionType st)
+    {
+      return [];
+    }
+  }
+}
--- cs-coding-questions/solutions/Solution.cs
+++ cs-coding-questions/solutions/Solution.cs
@@ -6,6 +6,7 @@ namespace cs_coding_questions.solutions
 {
   internal abstract class Solution(Dictionary<string, string> solutionParams, bool? debug = false)
   {
+    public bool paramsAreValid = false;
     public abstract bool verifyParams();
     public abstract string[] solve(SolutionType st);
     public void debugLog(string text)
