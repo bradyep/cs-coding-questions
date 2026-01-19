eabeefc65fcc26051a9c961d761cea3fdfcdbdc8
Brady Prigge <bradyep@gmail.com>
2026-01-18 20:34:12 -0600

    fix: move logic to Loader. Use records.

--- cs-coding-questions/Loader.cs
+++ cs-coding-questions/Loader.cs
@@ -5,12 +5,58 @@ using System.Text;
 
 namespace cs_coding_questions
 {
+  internal record ProcessArgsResult(bool Success, string Message);
   internal class Loader
   {
-    public Loader(string solutionName, SolutionType solutionType, string[] solutionArgs) {
-
+    public Loader(string[] args) {
+      this.processArgsResult = this.ProcessArgs(args);
+      Console.WriteLine($"Arg processing result: {this.processArgsResult.Success} | {this.processArgsResult.Message}");
     }
 
+    private string? solutionName;
+    private SolutionType solutionType;
+    private ProcessArgsResult processArgsResult;
+
+    private ProcessArgsResult ProcessArgs(string[] args)
+    {
+      if (args.Length < 2)
+      {
+        return new ProcessArgsResult(false, $"Need solution name and type to run. Exiting out.");
+      }
+      else
+      {
+        Console.WriteLine($"args[0]: {args[0]}, args[1]: {args[1]}");
+        this.solutionName = args[0];
+        int solutionTypeArg;
+        if (int.TryParse(args[1], out solutionTypeArg) == false)
+        {
+          return new ProcessArgsResult(false, $"Did not receive a number for SolutionType: {args[1]}");
+        }
+
+        if (Enum.IsDefined(typeof(SolutionType), solutionTypeArg))
+        {
+          this.solutionType = (SolutionType)solutionTypeArg;
+        }
+        else
+        {
+          return new ProcessArgsResult(false, $"Received invalid SolutionType: {solutionTypeArg}");
+        }
+
+        Console.WriteLine($"Solution: {this.solutionName} | SolutionType: {this.solutionType}");
+        return new ProcessArgsResult(true, "Successfully read in params");
+      }
+    }
+
+    public void Run()
+    {
+      if (this.processArgsResult.Success == false)
+      {
+        Console.WriteLine($"Could not process args. Shutting down.");
+
+        return;
+      }
+      return;
+    }
 
   }
 }
--- cs-coding-questions/Program.cs
+++ cs-coding-questions/Program.cs
@@ -1,37 +1,12 @@
-﻿using cs_coding_questions.solutions;
+﻿using cs_coding_questions;
+using cs_coding_questions.solutions;
 
 class Program
 {
   static void Main(string[] args)
   {
     Console.WriteLine($"Number of args: {args.Length}");
-
-    if (args.Length < 2)
-    {
-      Console.WriteLine($"Need solution name and type to run. Exiting out.");
-      Environment.Exit(0);
-    } else {
-      Console.WriteLine($"args[0]: {args[0]}, args[1]: {args[1]}");
-      var solutionName = args[0];
-      int solutionTypeArg;
-      SolutionType solutionType = 0;
-      if (int.TryParse(args[1], out solutionTypeArg) == false)
-      {
-        Console.WriteLine($"Did not receive a number for SolutionType: {args[1]}");
-        Environment.Exit(0);
-      }
-
-      if (Enum.IsDefined(typeof(SolutionType), solutionTypeArg))
-      {
-        solutionType = (SolutionType)solutionTypeArg;
-      }
-      else
-      {
-        Console.WriteLine($"Received invalid SolutionType: {solutionTypeArg}");
-        Environment.Exit(0);
-      }
-
-      Console.WriteLine($"Solution: {solutionName} | SolutionType: {solutionType}");
-    }
+    var loader = new Loader(args);
+    loader.Run();
   }
 }
